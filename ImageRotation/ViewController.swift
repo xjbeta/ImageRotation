fb5197b078a75343069d9c534ef5564e2718c8d0
xjbeta <xjbeta.tiny@gmail.com>
2019-09-06 12:23:59 +0800

    Initial Commit

---
+++ ImageRotation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageRotation
+//
+//  Created by xjbeta on 2019/9/6.
+//  Copyright © 2019 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
